cda96542f590b202dd8125bd6853f93f8366c3d9
Paul Walker <paul@pwjw.com>
2024-09-28 15:06:57 -0400

    Further simplify example

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 BD=`pwd`/bld
-mkdir ${BD}
-pushd lib/
-./build-macos-luajit.sh "${BD}"
+rm will_crash
+rm wont_crash
+rm -rf ${BD}
+mkdir -p ${BD}/luabld
+
+pushd lib/LuaJIT
+cp -r . ${BD}/luabld
 popd
 
-clang++ -o will_crash -Ibld/include -std=c++17 -O3 src/test.cpp -Lbld/bin -lluajit
-clang++ -o wont_crash -Ibld/include -std=c++17 -O3 src/test.cpp -Wl,-no_deduplicate -Lbld/bin -lluajit
+pushd ${BD}/luabld
+export MACOSX_DEPLOYMENT_TARGET=10.9
+make amalg TARGET_CCFLAGS="-O3"
+mkdir -p ../lib
+mv src/libluajit.a ../lib
+popd
+
+
+clang++ -o will_crash -Ibld/luabld/src -std=c++17 -O3 src/test.cpp -Lbld/lib -lluajit
+clang++ -o wont_crash -Ibld/luabld/src -std=c++17 -O3 src/test.cpp -Wl,-no_deduplicate -Lbld/lib -lluajit
 
